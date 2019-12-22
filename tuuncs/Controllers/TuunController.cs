cb8b3d4afd68e171bb9bd697aedbda1741a040be
Adam Wallacessh-keygen -t rsa -b 4096 -C awallace689@gmail.com <awallace689@gmail.com>
2019-12-22 14:21:55 -0600

    Spotify api proof of concept

--- tuuncs/Controllers/TuunController.cs
+++ tuuncs/Controllers/TuunController.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
 using SpotifyAPI.Web.Enums;
 using SpotifyAPI.Web.Models;
+using Secrets;
 
 namespace tuuncs.Controllers
 {
@@ -14,6 +17,7 @@ namespace tuuncs.Controllers
     public class TuunController : ControllerBase
     { 
         private readonly ILogger<Tuun> _logger;
+        private static SpotifyWebAPI _spotify;
 
         public TuunController(ILogger<Tuun> logger)
         {
@@ -31,13 +35,25 @@ namespace tuuncs.Controllers
         [Route("proof")]
         public async Task<IActionResult> proof()
         {
-            return Ok(await asyncFunction("This is my async string!"));
+            await initializeSpotifyService();
+            return Ok(await BlessTheRain());
         }
 
-        public async Task<string> asyncFunction(string str)
+
+        public async Task<string> BlessTheRain()
         {
-            string asyncStr = await Task.FromResult(str);
-            return asyncStr;
+            FullTrack track = await _spotify.GetTrackAsync("2374M0fQpWi3dLnB54qaLX");
+            return track.Name;
+        }
+        public async Task initializeSpotifyService()
+        {
+            CredentialsAuth auth = new CredentialsAuth(Secret._clientID, Secret._clientSecret);
+            Token token = await auth.GetToken();
+            _spotify = new SpotifyWebAPI()
+            {
+                AccessToken = token.AccessToken,
+                TokenType = token.TokenType
+            };
         }
     }
 }
