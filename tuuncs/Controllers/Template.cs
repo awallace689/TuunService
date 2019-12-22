9753981c383f0a3e16ec70ba6c73df6d3ff6f9ed
wittmanalex <wittmanalex@gmail.com>
2019-12-22 14:51:12 -0600

    Added template controller with database access.

---
+++ tuuncs/Controllers/Template.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SpotifyAPI.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Secrets;
+
+namespace tuuncs.Controllers
+{
+    [ApiController]
+    [Route("/Tuun/Template")]
+    public class TemplateController : ControllerBase
+    { 
+        private readonly ILogger<Tuun> _logger;
+
+        private MongoClient _client;
+        private IMongoDatabase _database;
+        private IMongoCollection<BsonDocument> _collection;
+
+        public TemplateController(ILogger<Tuun> logger)
+        {
+            _logger = logger;
+            _client = new MongoClient(Secret._MongoClient);
+            _database = _client.GetDatabase("Tuun");
+            _collection = _database.GetCollection<BsonDocument>("LiveRooms");
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return _collection.Find(new BsonDocument()).First().ToString();
+        }
+    }
+}
