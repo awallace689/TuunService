33c559bf2f46b8c37d29fff30c89903ef1f00449
Adam Wallacessh-keygen -t rsa -b 4096 -C awallace689@gmail.com <awallace689@gmail.com>
2019-12-21 18:49:08 -0600

    This is Tuun now!

---
+++ tuuncs/Controllers/Tuun.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace tuuncs.Controllers
+{
+    [ApiController]
+    [Route("/Tuun/proof")]
+    public class Tuun : ControllerBase
+    { 
+        private readonly ILogger<Tuun> _logger;
+
+        public Tuun(ILogger<Tuun> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return new string("This is Tuun now!");
+        }
+    }
+}
---
+++ tuuncs/Tuun.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace tuuncs
+{
+    public class Tuun
+    {
+
+    }
+}
