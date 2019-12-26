87005ce4582b8a3a2c0c34478c36f650be27db32
Adam Wallace <awallace689@gmail.com>
2019-12-26 15:09:51 -0600

    tuun/dbtracklist pulls list of song ids from database

--- tuuncs/Controllers/Template.cs
+++ tuuncs/Controllers/Template.cs
@@ -8,11 +8,14 @@ using SpotifyAPI.Web;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using Secrets;
+using MongoDB.Bson.Serialization.Attributes;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace tuuncs.Controllers
 {
     [ApiController]
-    [Route("/Tuun/Template")]
+    [Route("Tuun")]
     public class TemplateController : ControllerBase
     { 
         private readonly ILogger<Tuun> _logger;
@@ -30,9 +33,46 @@ namespace tuuncs.Controllers
         }
 
         [HttpGet]
+        [Route("template")]
         public string Get()
         {
             return _collection.Find(new BsonDocument()).First().ToString();
         }
+
+        [HttpGet]
+        [Route("dbTrackList")]
+        public IActionResult getList()
+        {
+            _collection = _database.GetCollection<BsonDocument>("Rooms");
+            var projection = Builders<BsonDocument>.Projection.Include("playlists").Exclude("_id");
+            var res = _collection.Find(new BsonDocument()).Project(projection).First();
+            if (res != null)
+            {
+                return Ok(JsonConvert.SerializeObject((res["playlists"].AsBsonArray)[0], Formatting.Indented));
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, res.ToString());
+            }
+        }
+
+      /*  class Room
+        {
+            public ObjectId id { get; set; }
+
+            [BsonElement("owner")]
+            public string owner { get; set; }
+
+            [BsonElement("active")]
+            public bool active { get; set; }
+
+            //Array<Array<string>>
+            [BsonElement("playlists")]
+            public Array playlists { get; set; }
+
+            //Array<string>
+            [BsonElement("members")]
+            public Array members { get; set; }
+        }*/
     }
 }
